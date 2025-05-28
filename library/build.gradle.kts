5c394d632fc1a4cb626161786d18bf3e7b3b9646
Maël GUERIN <mael.guerin@murena.io>
2025-05-28 14:58:09 +0200

    chore: version `1.0.0`

--- library/build.gradle.kts
+++ library/build.gradle.kts
@@ -74,8 +74,8 @@ android {
         minSdk = libs.versions.android.minSdk.get().toInt()
     }
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_11
-        targetCompatibility = JavaVersion.VERSION_11
+        sourceCompatibility = JavaVersion.VERSION_21
+        targetCompatibility = JavaVersion.VERSION_21
     }
 }
 
